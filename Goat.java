70733068a9a7fa0889e6ffdf25e2115507c6d3fc
LutragoacliffordIT24A <cliffordjohnburdeoslutrago@gmail.com>
2023-11-09 16:48:02 +0800

    Added Goat.java

---
+++ Goat.java
@@ -0,0 +1,12 @@
+
+
+
+public class Goat extends Animal{
+    Goat(String name){
+        super(name);
+        
+    }
+     void eat(){
+        System.out.println(name + "is eating grass");
+    }
+}
