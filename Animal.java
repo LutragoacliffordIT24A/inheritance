dcfc5a5b1394d7f536f283ecc6fdd886900b8a2d
LutragoacliffordIT24A <cliffordjohnburdeoslutrago@gmail.com>
2023-11-09 16:46:12 +0800

    Added Animal.java

---
+++ Animal.java
@@ -0,0 +1,16 @@
+
+
+
+public class Animal {
+    String name;
+    
+    Animal(String name){
+        this.name = name;
+    }
+    void sing(){
+        System.out.println(name + "is singing softly");
+    }
+     void runfast(){
+        System.out.println(name + "is running fast like a chetaah");
+}
+}
